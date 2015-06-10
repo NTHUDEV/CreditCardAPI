8dfd24a3d3d6ffc21ad9ea387df64f7811560551
csrordzhn <c_man182@yahoo.com>
2015-06-11 03:54:44 +0800

    2015-06-10: added json status msg for all paths

--- app.rb
+++ app.rb
@@ -23,13 +23,13 @@ get '/api/v1/index' do
     halt 200, CreditCard.all.to_json
 
   rescue Exception => e
-    halt 500, "All these moments will be lost in time like tears in the rain. -Roy Batty. Please punch your app dev in the face and show this #{e}."
+    halt 500, { status: "All these moments will be lost in time like tears in the rain. -Roy Batty. Please punch your app dev in the face and show this #{e}."}.to_json
   end
 end
 
 get '/api/v1/credit_card/validate' do
   content_type :json
-  halt 401, 'Unauthorized' unless authenticate(env['HTTP_AUTHORIZATION'])
+  halt 401, {status: 'Unauthorized'}.to_json unless authenticate(env['HTTP_AUTHORIZATION'])
   creditcard = CreditCard.new
   creditcard.number = params[:card_number]
   {card: creditcard.number, validated: creditcard.validate_checksum}.to_json
@@ -38,8 +38,8 @@ end
 get '/api/v1/credit_card' do
   begin
     content_type :json
-    halt 401, 'Unauthorized, info' unless authenticate(env['HTTP_AUTHORIZATION'])
-    halt 401, 'Unauthorized, user' unless @user_id == params[:user_id].to_i
+    halt 401, {status: 'Unauthorized, info'}.to_json unless authenticate(env['HTTP_AUTHORIZATION'])
+    halt 401, {status: 'Unauthorized, user'}.to_json unless @user_id == params[:user_id].to_i
     halt 200, CreditCard.where(user_id: params[:user_id]).to_json
 rescue Exception => e
   halt 500, "All these moments will be lost in time like tears in the rain. -Roy Batty. Please punch your app dev in the face and show this #{e}."
@@ -49,16 +49,16 @@ end
 post '/api/v1/credit_card' do
   begin
     content_type :json
-    halt 401, 'Unauthorized, info.' unless authenticate(env['HTTP_AUTHORIZATION'])
-    halt 401, 'Unauthorized, user' unless @user_id == params[:user_id].to_i
+    halt 401, {status: 'Unauthorized, info'}.to_json unless authenticate(env['HTTP_AUTHORIZATION'])
+    halt 401, {status: 'Unauthorized, user'}.to_json unless @user_id == params[:user_id].to_i
     request_json = request.body.read
     req = JSON.parse(request_json)
     creditcard = CreditCard.new(:number => req['card_number'].to_s,:expiration_date => req['expiration_date'].to_s,:owner => req['owner'].to_s,:credit_network => req['credit_network'].to_s, :user_id => @user_id)
-    halt 400, "Whoa! Did you made a mistake or are you trying to trick the system?" unless creditcard.validate_checksum
+    halt 400, {status: 'Whoa! Did you made a mistake or are you trying to trick the system?'}.to_json unless creditcard.validate_checksum
     creditcard.save
-    halt 201, "Welcome to the wonderful family of Enigma Mfg. We shall splurge on fancy equipment for our office with your credit card."
+    halt 201, {status: 'Welcome to the wonderful family of Enigma Mfg. We shall splurge on fancy equipment for our office with your credit card.'}.to_json
   rescue
-    halt 410, "I'm sorry Dave, I'm afraid I can't do that. -HAL9000"
+    halt 410, {status: "I'm sorry Dave, I'm afraid I can't do that. -HAL9000"}.to_json
   end
 end
 end
